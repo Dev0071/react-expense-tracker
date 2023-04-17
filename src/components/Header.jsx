1dd61831613e806b44c35a8575d6fed4a839518f
Dev0071 <lecturergacheru71@gmail.com>
2023-04-17 23:53:49 +0300

    implemented validation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { useGlobalContext } from '../context';
 
 const Header = () => {
+  const { balance, totalIncome, totalExpenses } = useGlobalContext();
+
+  const incomeSign = totalIncome >= 0 ? '+' : '-';
+  const incomeFormatted = totalIncome.toFixed(2);
+
+  const expensesSign = totalExpenses > 0 ? '+' : '-';
+
   return (
     <div className="flex flex-col">
       <h2 className="font-semibold mt-2 text-gray-800 text-center mb-4 text-lg ">
@@ -9,17 +16,23 @@ const Header = () => {
       </h2>
       <div className="mb-2">
         <p className="text-[15px] text-gray-800">Your Balance</p>
-        <p className="text-lg text-gray-800 font-semibold">$0</p>
+        <p className="text-lg text-gray-800 font-semibold">
+          ${balance.toFixed(2)}
+        </p>
       </div>
       <div className="w-full p-4 bg-white flex items-center justify-around shadow-md mb-7">
         <div className="flex flex-col justify-center items-center">
           <p className="uppercase text-sm text-gray-800">Income</p>
-          <p className="text-green-600">$500.00</p>
+          <p className="text-green-600">
+            {incomeSign}${incomeFormatted}
+          </p>
         </div>
         <div className="bg-gray-500 w-[1px] h-10 "></div>
         <div className="flex flex-col justify-center items-center">
           <p className="uppercase text-sm text-gray-800">Expense</p>
-          <p className="text-red-700">$300.00</p>
+          <p className="text-red-700">
+            {expensesSign}${Math.abs(totalExpenses).toFixed(2)}
+          </p>
         </div>
       </div>
     </div>
--- src/context.jsx
+++ src/context.jsx
@@ -1,4 +1,4 @@
-import react, { useContext, useReducer, useState } from 'react';
+import react, { useContext, useEffect, useReducer, useState } from 'react';
 import reducer from './reducer';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -16,9 +16,6 @@ const initialState = {
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const { transactionName, transactionAmount } = state;
-  const transaction = { transactionName, transactionAmount };
-
   // get the name of the transaction
   const getTransactionName = (e) => {
     dispatch({ type: 'GET_TRANSACTION_NAME', payload: e.target.value });
@@ -29,9 +26,81 @@ const AppProvider = ({ children }) => {
     dispatch({ type: 'GET_TRANSACTION_AMOUNT', payload: e.target.value });
   };
 
+  // add a new transaction
   const addTransaction = () => {
-    dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
+    const { transactionName, transactionAmount } = state;
+    const numTransactionAmount = Number(transactionAmount);
+
+    if (isNaN(numTransactionAmount) || transactionName === '') {
+      <p>hello</p>;
+      console.log('Transaction amount must be a number');
+      return;
+    }
+
+    if (numTransactionAmount === 0) {
+      console.log('amount shouldnt be zero');
+      return;
+    }
+
+    const NewTransaction = {
+      id: uuidv4(),
+      transactionName,
+      numTransactionAmount,
+    };
+    dispatch({ type: 'ADD_TRANSACTION', payload: NewTransaction });
   };
+
+  // total Income
+  useEffect(() => {
+    const income = state.transactions.reduce((total, transaction) => {
+      return transaction.numTransactionAmount > 0
+        ? total + transaction.numTransactionAmount
+        : total;
+    }, 0);
+    dispatch({ type: 'SET_TOTAL_INCOME', payload: income });
+  }, [state.transactions]);
+
+  // total Expenses
+  useEffect(() => {
+    const expenses = state.transactions.reduce((total, transaction) => {
+      return transaction.numTransactionAmount < 0
+        ? total + transaction.numTransactionAmount
+        : total;
+    }, 0);
+    dispatch({ type: 'SET_TOTAL_EXPENSES', payload: expenses });
+  }, [state.transactions]);
+
+  // total balance
+  useEffect(() => {
+    const income = state.transactions.reduce((total, transaction) => {
+      return transaction.numTransactionAmount > 0
+        ? total + transaction.numTransactionAmount
+        : total;
+    }, 0);
+    const expenses = state.transactions.reduce((total, transaction) => {
+      return transaction.numTransactionAmount < 0
+        ? total + transaction.numTransactionAmount
+        : total;
+    }, 0);
+    const balance = income + expenses;
+    dispatch({ type: 'SET_BALANCE', payload: balance });
+  }, [state.transactions]);
+
+  // store transactions in local storage
+  useEffect(() => {
+    localStorage.setItem('transactions', JSON.stringify(state.transactions));
+  }, [state.transactions]);
+
+  // retrieve transactions from local storage on app load
+  useEffect(() => {
+    const storedTransactions = localStorage.getItem('transactions');
+    if (storedTransactions) {
+      dispatch({
+        type: 'SET_TRANSACTIONS',
+        payload: JSON.parse(storedTransactions),
+      });
+    }
+  }, []);
 
   return (
     <AppContext.Provider
